17dda35586cae083caad231ab9321d7fce3e4e58
valtersamasgrau244 <valter.joao@escola.pr.gov.br>
2024-11-28 19:33:23 -0300

    .

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('motivacao-button').addEventListener('click', function() {
+    document.getElementById('motivacao-message').textContent = 'Você é capaz de alcançar todos os seus sonhos!';
+});
